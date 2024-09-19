8371d57ecc0c591a5a92eedaa9fc8359a5681fd0
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-09-19 11:21:49 -0400

    initialize road grid and send update messages

--- simCore.py
+++ simCore.py
@@ -11,11 +11,23 @@ SIMULATION_STEP = CONFIG['simulation']['step']
 DEFAULT_SIMULATION_DURATION = CONFIG['simulation']['duration']
 SQS_QUEUE_VEHICLE_UPDATES = CONFIG['sqs']['queue_vehicle_updates']
 SQS_QUEUE_TRAFFIC_UPDATES = CONFIG['sqs']['queue_traffic_updates']
+GRID_WIDTH = CONFIG['visualization']['grid_width']
+GRID_HEIGHT = CONFIG['visualization']['grid_height']
 
 class SimulationCore:
     def __init__(self):
         self.clock = 0
         self.road_network = {}
+        self.initialize_grid_roads()
+
+    def initialize_grid_roads(self):
+        # Create a grid of roads
+        for i in range(GRID_WIDTH):
+            for j in range(GRID_HEIGHT):
+                if i < GRID_WIDTH - 1:
+                    self.add_road(f"H{i}-{j}", (i, j), (i+1, j), 60)
+                if j < GRID_HEIGHT - 1:
+                    self.add_road(f"V{i}-{j}", (i, j), (i, j+1), 60)
 
     def add_road(self, road_id, start_node, end_node, speed_limit):
         self.road_network[road_id] = {
@@ -42,6 +54,9 @@ class SimulationCore:
         process_sqs_messages(SQS_QUEUE_VEHICLE_UPDATES, process_message)
 
     def run(self, duration=DEFAULT_SIMULATION_DURATION):
+        # Send initial road network information
+        self.send_road_network()
+
         start_time = time.time()
         while time.time() - start_time < duration:
             self.update()
@@ -52,8 +67,13 @@ class SimulationCore:
         road_info['available_roads'] = list(self.road_network.keys())
         return road_info
 
+    def send_road_network(self):
+        message = {
+            'type': 'road_network_update',
+            'roads': self.road_network
+        }
+        send_sqs_message(SQS_QUEUE_TRAFFIC_UPDATES, message)
+
 if __name__ == "__main__":
     sim = SimulationCore()
-    sim.add_road("R1", "A", "B", 60)
-    sim.add_road("R2", "B", "C", 40)
     sim.run()
