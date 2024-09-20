cb9f02e6dc154e11ee6ea14ff14c27e661fc0d24
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-09-20 18:13:22 -0400

    ctrl+c actually works now

--- scripts/run_simulation.sh
+++ scripts/run_simulation.sh
@@ -6,11 +6,31 @@
 # Navigate to the root directory of the project
 cd "$(dirname "$0")/.."
 
-# Run the modules
-python -m traffic_simulation.core.simCore &
-python -m traffic_simulation.core.agentModule &
-python -m traffic_simulation.core.trafficModule &
-# python -m traffic_simulation.core.visualizationModule &
+# Function to kill background processes on exit
+cleanup() {
+    echo "Stopping background processes..."
+    # Send SIGTERM to all child processes
+    pkill -P $$
+}
+
+# Trap SIGINT (Ctrl+C) and SIGTERM signals and call cleanup
+trap cleanup SIGINT SIGTERM
+
+# Run the modules in the background and store their PIDs
+python3 -m traffic_simulation.core.simCore &
+SIMCORE_PID=$!
+
+python3 -m traffic_simulation.core.agentModule &
+AGENT_PID=$!
+
+python3 -m traffic_simulation.core.trafficModule &
+TRAFFIC_PID=$!
+
+# Uncomment if you have the visualization module
+# python3 -m traffic_simulation.core.visualizationModule &
+# VISUALIZATION_PID=$!
 
 # Wait for all background processes to finish
-wait
+wait $SIMCORE_PID $AGENT_PID $TRAFFIC_PID
+# If visualization module is used, include its PID:
+# wait $SIMCORE_PID $AGENT_PID $TRAFFIC_PID $VISUALIZATION_PID
