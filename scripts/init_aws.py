d1389b5a187c1b7a899fdc7201e8f584dede0eee
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-09-21 17:20:51 -0400

    add more roads

--- scripts/init_aws.py
+++ scripts/init_aws.py
@@ -8,18 +8,18 @@ import json
 def generate_initial_state():
     # Define intersections
     intersections = pd.DataFrame({
-        'intersection_id': ['A', 'B', 'C'],
-        'x': [0, 1, 0],
-        'y': [0, 0, 1]
+        'intersection_id': ['A', 'B', 'C', 'D', 'E', 'F'],
+        'x': [0, 1, 2, 0, 1, 2],
+        'y': [0, 0, 0, 1, 1, 1]
     })
 
     # Define roads
     roads = pd.DataFrame({
-        'road_id': ['A-B', 'A-C', 'B-C'],
-        'start': ['A', 'A', 'B'],
-        'end': ['B', 'C', 'C'],
-        'length': [1.0, 1.0, 1.41],
-        'speed_limit': [50, 50, 40]
+        'road_id': ['A-B', 'B-C', 'D-E', 'E-F', 'A-D', 'B-E', 'C-F', 'A-E', 'B-D', 'C-E'],
+        'start': ['A', 'B', 'D', 'E', 'A', 'B', 'C', 'A', 'B', 'C'],
+        'end': ['B', 'C', 'E', 'F', 'D', 'E', 'F', 'E', 'D', 'E'],
+        'length': [1.0, 1.0, 1.0, 1.0, 1.0, 1.41, 1.41, 1.41, 1.41, 1.41],
+        'speed_limit': [50, 50, 50, 50, 50, 40, 40, 40, 40, 40]
     })
 
     # Merge to get start coordinates
@@ -36,22 +36,23 @@ def generate_initial_state():
 
     # Define traffic lights
     traffic_lights = pd.DataFrame({
-        'intersection_id': ['A', 'B', 'C'],
-        'state': ['green', 'red', 'yellow']
+        'intersection_id': ['A', 'B', 'C', 'D', 'E', 'F'],
+        'state': ['green', 'red', 'yellow', 'green', 'red', 'yellow']
     })
 
     # Define vehicles
     vehicles = pd.DataFrame({
-        'vehicle_id': [f'vehicle_{i}' for i in range(10)],
-        'road': ['A-B', 'A-C', 'B-C', 'A-B', 'A-C', 'B-C', 'A-B', 'A-C', 'B-C', 'A-B'],
-        'position': [0.0] * 10,
-        'speed': [20] * 10
+        'vehicle_id': [f'vehicle_{i}' for i in range(20)],
+        'road': ['A-B', 'B-C', 'D-E', 'E-F', 'A-D', 'B-E', 'C-F', 'A-E', 'B-D', 'C-E',
+                 'A-B', 'B-C', 'D-E', 'E-F', 'A-D', 'B-E', 'C-F', 'A-E', 'B-D', 'C-E'],
+        'position': [0.0] * 20,
+        'speed': [20] * 20
     })
 
     # Define road blockages
     road_blockages = pd.DataFrame({
-        'road_id': ['A-B', 'A-C', 'B-C'],
-        'blocked': [False, False, False]
+        'road_id': roads['road_id'],
+        'blocked': [False] * len(roads)
     })
 
     # List of DataFrames and corresponding file names
@@ -73,14 +74,14 @@ def generate_initial_state():
     s3 = boto3.client('s3')
 
     # Upload files to S3 and delete local copies
-    bucket_name = 'trafficsimulation'
+    bucket_name = 'trafficsimulation'  # Replace with your actual bucket name
     s3_links = {}
     for file_name in dataframes.keys():
         try:
             s3.upload_file(file_name, bucket_name, file_name)
             os.remove(file_name)
             print(f"Uploaded and deleted local file: {file_name}")
-            # Generate S3 link (assuming public access or appropriate permissions)
+            # Generate S3 link (assuming appropriate permissions)
             s3_link = f"s3://{bucket_name}/{file_name}"
             s3_links[file_name] = s3_link
             print(f"S3 Link: {s3_link}")
