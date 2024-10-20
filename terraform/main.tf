463158c5056e0a44c293a84b1a3914b9b9ea6602
mvilsoet@gmail.com <mvilsoet@gmail.com>
2024-10-20 15:56:02 -0400

    TF state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,23 @@
+# Store tf.state in S3
+terraform {
+  backend "s3" {
+    bucket = "mvilsoet-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create S3 Bucket
+# Create S3 Bucket for traffic simulation data
 resource "aws_s3_bucket" "traffic_simulation_bucket" {
   bucket = "trafficsimulation"
+}
+
+resource "aws_s3_bucket_acl" "traffic_simulation_bucket_acl" {
+  bucket = aws_s3_bucket.traffic_simulation_bucket.id
   acl    = "private"
 }
 
@@ -15,8 +28,8 @@ resource "aws_sqs_queue" "standard_queue" {
 
 # Create SQS FIFO Queue
 resource "aws_sqs_queue" "fifo_queue" {
-  name                      = "SimCoreUpdates.fifo"
-  fifo_queue                = true
+  name                        = "SimCoreUpdates.fifo"
+  fifo_queue                  = true
   content_based_deduplication = true
 }
 
